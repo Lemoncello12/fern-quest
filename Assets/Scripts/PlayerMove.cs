41c36290a043cffe4b912bef6e24fec3afdb5d45
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2024-03-18 08:59:16 -0700

    Made Health bar work

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,11 +6,16 @@ public class PlayerMove : MonoBehaviour
 {
     public float speed = 5f;
     public Rigidbody2D rb;
+    public int currentHealth = 0;
+    public int maxHealth = 20;
     Vector2 moving;
+
+    public HealthBar healthBar;
     // Start is called before the first frame update
     void Start()
     {
-
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -20,5 +25,16 @@ public class PlayerMove : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         moving = new Vector2(horizontal * speed, vertical * speed);
         rb.velocity = moving.normalized * speed;
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
