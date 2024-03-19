b8a234dae8a4e12f080d3bffb79335281b38268b
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2024-03-19 10:04:36 -0700

    Finished damage system, started game over screen

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerMove : MonoBehaviour
 {
+
+
     public float speed = 5f;
     public Rigidbody2D rb;
     public int currentHealth = 0;
@@ -26,15 +28,23 @@ public class PlayerMove : MonoBehaviour
         moving = new Vector2(horizontal * speed, vertical * speed);
         rb.velocity = moving.normalized * speed;
 
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            TakeDamage(1);
-        }
     }
 
-    void TakeDamage(int damage)
+    public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            OnDeath();
+        }
+    }
+
+    void OnDeath()
+    {
+        Time.timeScale = 0f;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
