cf79d76638b2e5f059029c26ed95a77e400b256b
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2024-07-12 09:30:58 -0700

    Imported all fern sprites

--- Assets/Scripts/DealDamage.cs
+++ Assets/Scripts/DealDamage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DealDamage : MonoBehaviour
 {
     public int damage = 4;
+    public float health = 10f;
     // Start is called before the first frame update
     void Start()
     {
