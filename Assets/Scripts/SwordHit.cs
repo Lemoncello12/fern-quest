bbf9840961d2a7b7cc47e956acc26e90630f68a8
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2024-07-14 09:13:59 -0700

    Finished sword mechanics

--- Assets/Scripts/SwordHit.cs
+++ Assets/Scripts/SwordHit.cs
@@ -1,26 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SwordHit : MonoBehaviour
 {
     DealDamage enemy;
     public float strength;
+    public float damageTime = 0.25f;
+    public float cooldownTime = 2f;
+    float timer;
+    bool damageTimer = false;
+    bool cooldown = false;
+    SpriteRenderer sprite;
+    BoxCollider2D collider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (damageTimer)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                damageTimer = false;
+                endHit();
+            }
+        }
+        if (cooldown)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                cooldown = false;
+            }
+        }
+       if (Input.GetMouseButtonDown(0) && !cooldown)
+        {
+            startHit();
+        }
+    }
+    
+    void startHit()
+    {
+        timer = damageTime;
+        damageTimer = true;
+        sprite.enabled = true;
+        collider.enabled = true;
     }
 
-    public void Rotate(float angle)
+    void endHit()
     {
-        
+        sprite.enabled = false;
+        collider.enabled = false;
+        timer = cooldownTime;
+        cooldown = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
