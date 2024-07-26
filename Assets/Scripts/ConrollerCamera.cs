24016f415f6b8ed63572f8e96c5eb4ed5ab1c66b
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2024-07-26 08:13:02 -0700

    Started working on camera

---
+++ Assets/Scripts/ConrollerCamera.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConrollerCamera : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private float currentPosY;
+    private Vector3 velocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, currentPosY, transform.position.z), ref velocity, speed * Time.deltaTime);
+
+
+    }
+
+    public void MoveToNewRoom(Transform newRoom)
+    {
+        currentPosX = newRoom.position.x;
+        currentPosY = newRoom.position.y;
+    }
+}
